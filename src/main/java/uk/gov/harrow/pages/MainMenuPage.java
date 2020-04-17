585d1ede3d2ff5f71cfe1905f91b9743fd25871f
Pratiksha <pratikshapatel1182@gmail.com>
2020-04-17 23:08:09 +0100

    harrow council

---
+++ src/main/java/uk/gov/harrow/pages/MainMenuPage.java
@@ -0,0 +1,12 @@
+package uk.gov.harrow.pages;
+
+import org.openqa.selenium.By;
+import uk.gov.harrow.utility.Utility;
+
+public class MainMenuPage extends Utility {
+    By mainMenuAerialLabel = By.xpath("//a[contains(text(),'Benefits')]");
+
+    public void mouseHoverOnBenefitsAndClick(){
+        waitUntilElementToBeClickable(mainMenuAerialLabel,20);
+        clickOnElement(mainMenuAerialLabel);}
+}
